7bb4ba0261006116fad141973e38ee32616f33d1
Stephen Locker <slocker3@gmail.com>
2019-11-11 16:36:12 -0800

    Group featered-image with page header

---
+++ templates/single.php
@@ -0,0 +1,41 @@
+<?php /* Template Name: Single */ ?>
+
+<?php get_header(); ?>
+
+<?php do_action( 'spine_theme_template_before_main', 'single.php' ); ?>
+
+<main id="wsuwp-main" class="spine-single-template">
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+		<?php do_action( 'spine_theme_template_before_headers', 'single.php' ); ?>
+
+		<?php wsuwp_spine_get_template_part( 'single.php', 'parts/headers' ); ?>
+
+		<?php do_action( 'spine_theme_template_after_headers', 'single.php' ); ?>
+
+		<?php do_action( 'spine_theme_template_before_content', 'single.php' ); ?>
+
+		<?php do_action( 'spine_theme_template_before_articles', 'single.php' ); ?>
+
+		<?php get_template_part( 'articles/article' ); ?>
+
+		<?php do_action( 'spine_theme_template_after_articles', 'single.php' ); ?>
+
+		<?php
+endwhile;
+endif; ?>
+
+<?php do_action( 'spine_theme_template_after_content', 'single.php' ); ?>
+
+<?php do_action( 'spine_theme_template_before_footer', 'single.php' ); ?>
+
+<?php wsuwp_spine_get_template_part( 'single.php', 'parts/footers' ); ?>
+
+<?php do_action( 'spine_theme_template_after_footer', 'single.php' ); ?>
+
+</main>
+
+<?php do_action( 'spine_theme_template_after_main', 'single.php' ); ?>
+
+<?php get_footer();
