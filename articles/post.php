f38599c6c074fe60fa5e7f7751234d0ba9847268
Jeremy Felt <jeremy.felt@gmail.com>
2019-12-02 12:44:21 -0800

    Adjust formatting of a `wp_link_pages()` call

--- articles/post.php
+++ articles/post.php
@@ -72,10 +72,12 @@
 		<div class="article-body">
 			<?php the_content(); ?>
 			<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'spine' ),
-				'after' => '</div>',
-			) );
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'spine' ),
+					'after' => '</div>',
+				)
+			);
 			?>
 		</div>
 	<?php endif; ?>
